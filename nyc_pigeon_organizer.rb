f74611f290f17eb3e19b6aa3fdd27bc832c4f934
Chaim Bryski <luzybry94@gmail.com>
2020-05-27 17:04:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,9 +2,22 @@ require "pry"
 
 def nyc_pigeon_organizer(data)
   new_hash = {}
-  data.each do |keys, values|
-    values[:purple].each do |name|
-      new_hash[:name] = :keys
+  data.each do |key, value|
+    binding.pry
+    value.each do |new_value, names|
+      names.each do |name|
+        
+        if !new_hash[name]
+          new_hash[name] = {}
+        end
+
+        if !new_hash[name][key]
+          new_hash[name][key] = []
+        end
+
+        new_hash[name][key] << new_value.to_s
+
+      end
     end
   end
   new_hash
