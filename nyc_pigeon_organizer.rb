8de322dcf55fec6d970dbbcc217799e3731a829c
Chaim Bryski <luzybry94@gmail.com>
2020-05-27 17:02:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |keys, values|
     values[:purple].each do |name|
-      new_hash[:name] = keys
+      new_hash[:name] = :keys
     end
   end
   new_hash
